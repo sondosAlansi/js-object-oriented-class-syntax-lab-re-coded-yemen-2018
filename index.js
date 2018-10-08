82dda776de3252799d72cfc5b43bad87a6e3d3f9
sondos alansi <sondos.alansi@yahoo.com>
2018-10-08 19:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class BoardMember()
 {
-  constructor(name, homeState, training)
+  constructor(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
@@ -8,18 +8,19 @@ class BoardMember()
    veto (){
      return 'No, I must disagree'
   }
-   BoardMember.prototype.approve = function(){
+  approve(){
     return 'You can do that!'
   }
-   BoardMember.prototype.doCharity = function(){
+   doCharity(){
     return "I like to help people."
   }
 
-   BoardMember.prototype.releasePressStatement = function(){
+  releasePressStatement(){
     return "You will see great things from Scuber."
   }
 
-  BoardMember.prototype.sayHi = function(){
+  sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
  
+}
